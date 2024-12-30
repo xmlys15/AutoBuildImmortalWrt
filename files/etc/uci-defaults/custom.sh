5eb92a48f96e097ceaf600a06bd9a43f617d0af4
xmlys15 <143062621+xmlys15@users.noreply.github.com>
2024-12-30 23:09:00 +0800

    Update custom.sh
    修改lan地址

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -4,9 +4,9 @@
 uci set firewall.@zone[1].input='ACCEPT'
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
+#uci add dhcp domain
+#uci set "dhcp.@domain[-1].name=time.android.com"
+#uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 
 # 根据网卡数量配置网络
@@ -23,7 +23,7 @@ done
 if [ "$count" -eq 1 ]; then
   uci set network.lan.proto='dhcp'
 elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.100.1'
+  uci set network.lan.ipaddr='192.168.2.22'
 fi
 
 # 设置所有网口可访问网页终端
